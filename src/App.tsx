7d1fd46ac681f656ef3627a0ae01ac875f7f305f
tuanndph16237 <tuanndph16237@example.com>
2022-12-12 18:03:54 +0700

    review-one left

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Route, Routes } from "react-router-dom";
+const Card = React.lazy(() => import("./components/Card"));
+const Create = React.lazy(() => import("./components/Create"));
+const Destinations = React.lazy(() => import("./components/Destinations"));
+const Details = React.lazy(() => import("./components/Details"));
+const NotFound = React.lazy(() => import("./components/NotFound"));
+const ReactQueryDestinations = React.lazy(
+  () => import("./components/ReactQueryDestinations")
+);
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<p>Loading component...</p>}>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/create" element={<Create />} />
+          <Route path="/hotels/:id" element={<Details />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Card />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/react-query" element={<ReactQueryDestinations />} />
+        </Routes>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Suspense>
   );
 }
 
